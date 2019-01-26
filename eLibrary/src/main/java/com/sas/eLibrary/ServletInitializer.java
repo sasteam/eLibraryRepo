6db9c9268ebd8951b557a0ff8e4eb7e5bb465aac
suman sourabh satpathy <46081174+suman104@users.noreply.github.com>
2019-01-27 00:20:24 +0530

    project created

---
+++ eLibrary/src/main/java/com/sas/eLibrary/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.sas.eLibrary;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ELibraryApplication.class);
+	}
+
+}
+
